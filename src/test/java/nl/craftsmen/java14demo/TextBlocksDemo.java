23e90405a15b77a015901a5fa3468cc90777adb9
MichelSchudel <michel.schudel@gmail.com>
2020-06-13 16:12:09 +0200

    old and new text blocks

--- src/test/java/nl/craftsmen/java14demo/TextBlocksDemo.java
+++ src/test/java/nl/craftsmen/java14demo/TextBlocksDemo.java
@@ -5,17 +5,29 @@ import org.junit.jupiter.api.Test;
 
 public class TextBlocksDemo {
 
+
+
+//    String html = "<html>\n" +
+//                  "    <body>\n" +
+//                  "        <p class=\"title\">Hello, Craftsmen</p>\n" +
+//                  "    </body>\n" +
+//                  "</html>\n";
+
+
+
     String html = """
-                <html>
-                <body>
-                    <p>Hello, Craftsmen</p>
-                </body>
-            </html>\"""
-                        """;
+                  <html>
+                      <body>
+                          <p class="title">Hello, Craftsmen</p>
+                      </body>
+                  </html>
+                  """;
 
     @Test
     public void textBlocks() {
+        System.out.println("----start------");
         System.out.println(html);
+        System.out.println("----end--------");
     }
 
 }
